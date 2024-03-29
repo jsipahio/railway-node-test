c1ad23733afd56145c844388d577aeff482e6e63
jsipahio <jsipahio@kent.edu>
2024-03-28 21:33:56 -0400

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const pokedex = require('./scripts/pokedata');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const pokeRouter = require('./scripts/pokerouter');
 
+app.use(cors());
 app.use('/static', express.static(path.join(__dirname,'public')));
 app.use(express.json());
 pokeRouter.getAllPokemon(pokedex, app);
